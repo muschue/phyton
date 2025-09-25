89bd8d2148d18c4010b8373b7e5c610c075a4fec
Oli delUna <oli@daa.de>
2025-09-25 12:43:43 +0200

    ein code mit variablen erstellt

---
+++ box.py
@@ -0,0 +1,8 @@
+#Define multiple variables and use them in a string
+name = "Oli"
+age = 40
+country = "DE"
+#In Varialbe result the string "Hello my name is Oli and I am 40 from DE." will be stored
+result = f"Hello my name is {name} and I am {age} from {country}."
+#Print the content of the variable result
+print(result)
